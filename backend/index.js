db5d461cf3bed8342f64cba3378bab2f669121b1
Paula Fraga <paula.fragapi@elorrieta-errekamari.com>
2025-03-25 21:22:25 +0100

    funcionalidad de registro y login + navBar

--- backend/index.js
+++ backend/index.js
@@ -50,3 +50,74 @@ app.post("/login", (req, res) => {
     }
   });
 });
+app.post("/erregistratu", (req, res) => {
+  const {
+    izena_abizena,
+    email,
+    pasahitza,
+    erabiltzailea,
+    jaiotza_data,
+    helbidea,
+  } = req.body;
+
+
+  if (
+    !izena_abizena ||
+    !email ||
+    !pasahitza ||
+    !erabiltzailea ||
+    !jaiotza_data
+  ) {
+    return res.status(400).json({ message: "Datu guztiak bete behar dira" });
+  }
+
+  const query =
+    "INSERT INTO USER (izena_abizena, email, pasahitza, erabiltzailea, jaiotza_data, helbidea) VALUES (?, ?, ?, ?, ?, ?)";
+
+  db.query(
+    query,
+    [
+      izena_abizena,
+      email,
+      pasahitza,
+      erabiltzailea,
+      jaiotza_data,
+      helbidea || null,
+    ],
+    (err, results) => {
+      if (err) {
+        console.error("Errorea erabiltzailea sortzean:", err);
+        return res.status(500).json({ message: "Errorea zerbitzarian" });
+      }
+
+      const userBerria = {
+        id: results.insertId,
+        izena_abizena,
+        email,
+        erabiltzailea,
+        jaiotza_data,
+        helbidea,
+      };
+
+      return res.status(201).json({ message: "Sartu da ondo", user: userBerria });
+    }
+  );
+});
+
+app.get("/get-users", (req, res) => {
+  const query = "SELECT * FROM user";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Errorea erabiltzaileak jasotzean:", err);
+      return res.status(500).json({ message: "Errorea zerbitzarian" });
+    }
+
+    return res.status(200).json({ users: results });
+  });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Zerbitzaria http://localhost:${PORT}-n martxan dago`);
+});
